5ec292c44237209eeebbf686758c33f3a60abb8a
Brandon Murambinda <mura0005@hz.nl>
2024-07-01 01:29:35 +0200

    419 error fix

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -21,4 +22,9 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+Route::post('/logout', function (Request $request) {
+    Auth::logout();
+    $request->session()->invalidate();
+    $request->session()->regenerateToken();
+    return redirect('/login'); // Redirect to homepage or any desired route after logout
+})->name('logout');
